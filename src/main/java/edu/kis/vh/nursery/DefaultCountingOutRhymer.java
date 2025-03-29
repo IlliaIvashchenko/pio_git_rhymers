9fda49884c4d125003b7366b60a9607c679c8ac9
mikita <ofprom101@gmail.com>
2025-03-29 17:42:29 +0100

    stale i adnotacje

---
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -0,0 +1,37 @@
+package edu.kis.vh.nursery;
+
+public class DefaultCountingOutRhymer {
+
+    public static final int INT = 12;
+    public static final int INT1 = -1;
+    public static final int INT2 = 11;
+    private int[] NUMBERS = new int[INT];
+
+    public int total = INT1;
+
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == INT1;
+    }
+
+    public boolean isFull() {
+        return total == INT2;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return INT1;
+        return NUMBERS[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return INT1;
+        return NUMBERS[total--];
+    }
+
+}
