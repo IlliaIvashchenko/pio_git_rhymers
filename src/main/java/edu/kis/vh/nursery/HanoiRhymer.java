99bbf83c38ba640c9ef002708286eeddef3cd6f0
mikita <ofprom101@gmail.com>
2025-03-30 09:17:55 +0200

    poprawki 2

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    public static final int INT3 = 0;
-    int totalRejected = INT3;
+    public static final int INITIAL_REJECTED_COUNT = 0;
+    int totalRejected = INITIAL_REJECTED_COUNT;
 
     public int reportRejected() {
         return totalRejected;
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,8 +2,10 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    public static final int DEFAULT_RETURN_VALUE = -1;
+
     Node last;
-    int i;
+    int i; // Consider removing if unused
 
     public void push(int i) {
         if (last == null)
@@ -25,16 +27,15 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return DEFAULT_RETURN_VALUE;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return DEFAULT_RETURN_VALUE;
         int ret = last.value;
         last = last.prev;
         return ret;
     }
-
 }
